23c0694d5e8b942884d7d9230ed58b8f055deef9
Bahana Alana Khansa <alanabahana102@gmail.com>
2022-04-10 13:16:32 +0700

    input argument implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,15 @@
-use std::{error::Error, fs, result};
+use std::{error::Error, fs, result, env};
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
 
-    for line in search(&config.query, &contents){
+    let results = if config.case_sensitive {
+        search(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
+
+    for line in results{
         println!("{}", line);
     }
     Ok(())
@@ -11,7 +17,8 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 
 pub struct Config {
     pub query: String, 
-    pub filename: String
+    pub filename: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -23,7 +30,13 @@ impl Config {
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Ok(Config {query, filename})
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
